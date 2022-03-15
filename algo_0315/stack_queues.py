7b22a7b8e93e9b3156adc32a48656e73bdb59639
ggongdori <soominlee0410@gmail.com>
2022-03-15 21:17:25 +0900

    stack_queue 수정 및 주석

--- algo_0315/stack_queues.py
+++ algo_0315/stack_queues.py
@@ -1,7 +1,9 @@
 class MyQueue:
 
     def __init__(self):
+        # input stack
         self.stack1 = []
+        # output stack
         self.stack2 = []
 
     def push(self, x: int) -> None:
@@ -10,7 +12,11 @@ class MyQueue:
     def pop(self) -> int:
 
     def peek(self) -> int:
-        return self.stack1[0]
+        # output이 없으면
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
 
     def empty(self) -> bool:
         return len(self.stack1) == 0 and len(self.stack2) == 0
