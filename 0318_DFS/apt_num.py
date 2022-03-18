bf16d679b3257de3ec9d428e5c675846ec5cad10
ggongdori <soominlee0410@gmail.com>
2022-03-18 11:31:12 +0900

    단지번호 붙이기 dfs

---
+++ 0318_DFS/apt_num.py
@@ -0,0 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+graph = [list(map(int, input().rstrip())) for _ in range(N)]
+#이번 문제에서는 사실 필요 없음
+visited = [[0]*N for _ in range(N)]
+ans = []
+cnt = 0
+
+def dfs(x,y):
+    global cnt
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    graph[x][y] = 0
+    cnt+=1
+    for i in range(4):
+        nx = x + dx[i] #i=0일때 (nx,ny)는 좌 , i=1일때 (nx,ny)는 상
+        ny = y + dy[i]
+        if nx < 0 or nx >= N or ny < 0 or ny >=N:
+            continue
+        if graph[nx][ny] == 1:
+            dfs(nx, ny)
+
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1:
+            cnt = 0
+            dfs(i,j)
+            ans.append(cnt)
+
+print(len(ans))
+ans.sort()
+for a in ans:
+    print(a)
