276eefb9f1bda0c20789069562ea9a7c709b501a
ggongdori <soominlee0410@gmail.com>
2022-03-22 14:10:33 +0900

    백준 parents_BT solution added

--- BT_0322/boj_parents_BT.py
+++ BT_0322/boj_parents_BT.py
@@ -1,11 +1,24 @@
 import sys
-
+from collections import deque
 input = sys.stdin.readline
 
 N = int(input())
-node = []
+
+#1번부터 시작하므로
+node = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
 for _ in range(N-1):
     n, m = map(int, input().split())
-    node.append((n, m))
+    node[n].append(m)
+    node[m].append(n)
 
-print(node)
+def Dfs(start, node, visited):
+    for i in node[start]:
+        if visited[i] == 0:
+            visited[i] = start
+            Dfs(i, node, visited)
+
+Dfs(1, node, visited)
+
+for i in range(2, N+1):
+    print(visited[i])
