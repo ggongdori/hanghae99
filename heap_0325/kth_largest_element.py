e62d3976f18d30fc8a0d1e36afd75e043f2c17f3
ggongdori <soominlee0410@gmail.com>
2022-03-25 11:26:53 +0900

    kth element solution added

--- heap_0325/kth_largest_element.py
+++ heap_0325/kth_largest_element.py
@@ -1,5 +1,6 @@
 import heapq
 
+#힙 없이
 def findKthLargest(self, nums, k):
     return sorted(nums, reverse=True)[k-1]
 
@@ -7,12 +8,22 @@ def findKthLargest(self, nums, k):
 
 def findKthLargest(self, nums, k):
     heap = []
+    #숫자들을 힙에 넣어줌
+    #최소 힙이기에 가장 작은 수가 루트 노드
     for num in nums:
         heapq.heappush(heap, num)
+    #k번째 큰 수 =
     for _ in range(len(nums)-k):
         heapq.heappop(heap)
     return heapq.heappop(heap)
 
+def findKthLargest(self, nums: List[int], k: int) -> int:
+    heapq.heapify(nums)
+    length = len(nums)
+    while length > k:
+        heapq.heappop(nums)
+        length -= 1
+    return nums[0]
 
 
 
