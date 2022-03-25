6c6a8a8846e9c6e256a7b3f4eee88025b9969936
ggongdori <soominlee0410@gmail.com>
2022-03-25 20:27:08 +0900

    insertion_sort solution added

---
+++ bubble_sort_0326/insertion_sort_list.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def insertionSortList(self, head):
+    curr = dummy = ListNode(0)
+    while head:
+        if curr and cur.val > head.val:
+            curr = dummy
+        while curr.next and curr.next.val < head.val: # classic insertion sort to find position
+            curr = curr.next
+        curr.next, curr.next.next, head = head, curr.next, head.next # insert
+    return dummy.next
