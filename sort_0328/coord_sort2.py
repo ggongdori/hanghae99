b9692f8c19c15715c1a9f3e29762e245848b8553
ggongdori <soominlee0410@gmail.com>
2022-03-28 09:38:21 +0900

    coord_sort2 solution added

---
+++ sort_0328/coord_sort2.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+coord = []
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    coord.append((x,y))
+
+# print(coord)
+coord.sort(key = lambda x: x[1])
+
+for c in coord:
+    print(c[0], c[1], end  = "\n")
