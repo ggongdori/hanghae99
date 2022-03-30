339c262d03a90ba09d7b2179d13d3e78f44dee6d
ggongdori <soominlee0410@gmail.com>
2022-03-30 09:27:33 +0900

    heap sort 0330 initial commit

--- merge_sort_0329/walls_and_gates.py
+++ merge_sort_0329/walls_and_gates.py
@@ -9,13 +9,13 @@ class Solution(object):
         if not rooms or not rooms[0]:
             return
 
-        numRows = len(rooms)
-        numCols = len(rooms[0])
+        rows = len(rooms)
+        cols = len(rooms[0])
 
         currLevel = []
 
-        for x in range(numRows):
-            for y in range(numCols):
+        for x in range(rows):
+            for y in range(cols):
                 if rooms[x][y] == gate:
                     currLevel.append( (x,y) )
 
@@ -26,7 +26,7 @@ class Solution(object):
                 currDist = rooms[x][y]
 
                 for nx,ny in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
-                    if 0<=nx<numRows and 0<=ny<numCols and rooms[nx][ny] == unknown:
+                    if 0<=nx<rows and 0<=ny<cols and rooms[nx][ny] == unknown:
                         rooms[nx][ny] = currDist + 1
                         nextLevel.append( (nx,ny) )
 
