885054c8b0f9978bed7c50ba14f3045c4049dfad
ggongdori <soominlee0410@gmail.com>
2022-03-30 11:00:34 +0900

    num_sort solution added

---
+++ heap_sort_0330/num_sort2.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+numbers = []
+for _ in range(N):
+    numbers.append(int(input()))
+
+numbers.sort()
+
+for num in numbers:
+    print(num, end = '\n')
