0521ff4b0ce4cde43896272ab29df79672404863
ggongdori <soominlee0410@gmail.com>
2022-03-30 11:01:11 +0900

    age_sort, card_sort solution exchanged

---
+++ heap_sort_0330/age_sort.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+members = []
+
+for _ in range(N):
+    age, name = map(str, input().split())
+    members.append((age, name))
+
+members.sort(key=lambda x: x[0])
+
+for m in members:
+    print(m[0], m[1], end = "\n")
