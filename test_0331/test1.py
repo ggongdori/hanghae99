185aa6dab6636ba9e40c0c7420d42aea7a2b6390
ggongdori <soominlee0410@gmail.com>
2022-03-31 14:12:22 +0900

    test1 solution added

--- test_0331/test1.py
+++ test_0331/test1.py
@@ -1,2 +1,22 @@
 import sys
 
+
+def solution(files):
+    answer = []
+
+    for f in files:
+        head, number, tail = '', '', ''
+        flag = False
+        for i in range(len(f)):
+            if f[i].isdigit():
+                number += f[i]
+                flag = True
+            elif not flag:
+                head += f[i]
+            else:
+                tail = f[i:]
+                break
+        answer.append((head, number, tail))
+
+    answer.sort(key=lambda x: (x[0].lower(), int(x[1])))
+    return [''.join(x) for x in answer]
