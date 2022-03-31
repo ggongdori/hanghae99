6f8778ebc95954b23c956b0654b0000d611c98c3
ggongdori <soominlee0410@gmail.com>
2022-03-31 14:41:09 +0900

    solutions modified

--- test_0331/test2.py
+++ test_0331/test2.py
@@ -1,8 +1,6 @@
-import sys
-
 def solution(citations):
     citations.sort()
     for idx , citation in enumerate(citations):
-        if citation >= len(citations) - idx :
+        if citation >= len(citations) - idx:
             return len(citations) - idx
     return 0
