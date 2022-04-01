bf44dc6ce638b992bd32359413a66c53e8bae021
ggongdori <soominlee0410@gmail.com>
2022-04-01 10:45:22 +0900

    02 solution added

--- bisect_0401/bisect_02.py
+++ bisect_0401/bisect_02.py
@@ -1,7 +1,23 @@
-import sys
+def binary_search(array,left,right,target):
+    while left<=right:
+        mid = (left+right)//2
+        if array[mid]==target:
+            return mid
+        elif array[mid]<target:
+            left=mid+1
+        elif array[mid]>target:
+            right=mid-1
+    return None
 
-input = sys.stdin.readline
+n = int(input())
+arr1 = list(map(int, input().split()))
+arr1.sort()
+m = int(input())
+arr2 = list(map(int, input().split()))
 
-N = int(input())
-
-
+for i in arr2:
+    x = binary_search(arr1,0,n-1,i)
+    if x != None:
+        print('yes',end=' ')
+    else:
+        print('no',end=' ')
