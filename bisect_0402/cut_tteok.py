47504bb7792537f66bae4491493783606a6fb760
ggongdori <soominlee0410@gmail.com>
2022-04-02 10:22:49 +0900

    떡 만들기 solution

--- bisect_0402/cut_tteok.py
+++ bisect_0402/cut_tteok.py
@@ -4,3 +4,21 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
+tteok = list(map(int, input().split()))
+
+start = 0
+end = max(tteok)
+answer = 0
+while start <= end:
+    total = 0
+    mid = (start+end) // 2
+    for t in tteok:
+        if t > mid:
+            total += t - mid
+    if total < m:
+        end = mid - 1
+    else:
+        answer = mid
+        start = mid + 1
+
+print(answer)
