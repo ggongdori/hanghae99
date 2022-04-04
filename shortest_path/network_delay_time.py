928743c08ce6b26718cde83ab2ff50ce77b57474
ggongdori <soominlee0410@gmail.com>
2022-04-04 11:11:04 +0900

    network_delay_time.py heapq solution added

--- shortest_path/network_delay_time.py
+++ shortest_path/network_delay_time.py
@@ -16,3 +16,23 @@ class Solution:
                     queue.append((time + w, v))
         mx = max(t)
         return mx if mx < float("inf") else -1
+
+#using heapq
+def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+    graph = collections.defaultdict(list)
+    for (u, v, w) in times:
+        graph[u].append((v, w))
+
+    priority_queue = [(0, K)]
+    shortest_path = {}
+    while priority_queue:
+        w, v = heapq.heappop(priority_queue)
+        if v not in shortest_path:
+            shortest_path[v] = w
+            for v_i, w_i in graph[v]:
+                heapq.heappush(priority_queue, (w + w_i, v_i))
+
+    if len(shortest_path) == N:
+        return max(shortest_path.values())
+    else:
+        return -1
