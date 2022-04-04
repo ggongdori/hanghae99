b7426660b9dc8af1c7eecf62f99694bf400f8650
ggongdori <soominlee0410@gmail.com>
2022-04-04 09:51:20 +0900

    0404 initial commit

--- bisect_0402/cut_tteok.py
+++ bisect_0402/cut_tteok.py
@@ -4,11 +4,14 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
+#가지고 있는 떡 길이 입력 받기
 tteok = list(map(int, input().split()))
 
+#최소, 최대 길이 설정
 start = 0
 end = max(tteok)
 answer = 0
+#최소가 최대보다 크기 전까지
 while start <= end:
     total = 0
     mid = (start+end) // 2
