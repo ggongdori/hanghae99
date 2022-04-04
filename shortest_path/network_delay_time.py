450e1a31796704630e7a9900a32022ed973afe2e
ggongdori <soominlee0410@gmail.com>
2022-04-04 10:40:12 +0900

    network_delay_time.py solution added

--- shortest_path/network_delay_time.py
+++ shortest_path/network_delay_time.py
@@ -1,1 +1,16 @@
-import sys
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        from collections import deque, defaultdict
+        t = [0] + [float("inf")] * n
+        graph = defaultdict(list)
+        queue = deque([(0, k)])
+        for u, v, w in times:
+            graph[u].append((v, w))
+        while queue:
+            time, node = q.popleft()
+            if time < t[node]:
+                t[node] = time
+                for v, w in graph[node]:
+                    queue.append((time + w, v))
+        mx = max(t)
+        return mx if mx < float("inf") else -1
