0bf72938628a7597da98a70215db8d909ea2e025
ggongdori <soominlee0410@gmail.com>
2022-04-05 10:58:53 +0900

    exercise solution added

---
+++ shortest_path_0405/exercise.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+INF = sys.maxsize
+
+v, e = map(int, input().split())
+graph = [[INF] * (v+1) for _ in range(v+1)]
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for k in range(1, v+1):
+    for i in range(1, v+1):
+        for j in range(1, v+1):
+            if graph[i][k] + graph[k][j] < graph[i][j]:
+                graph[i][j] = graph[i][k] + graph[k][j]
+
+answer = INF
+for i in range(1, v+1):
+    answer = min(answer, graph[i][i])
+
+if answer == sys.maxsize:
+    print(-1)
+else:
+    print(answer)
+
