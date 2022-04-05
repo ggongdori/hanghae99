cf59883b30a7518899cd5afc2c9c5f63e29f84fb
ggongdori <soominlee0410@gmail.com>
2022-04-05 10:47:47 +0900

    shortest_path solution

---
+++ shortest_path_0405/shortest_path_boj.py
@@ -0,0 +1,38 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+INF = 1e9
+v, e = map(int, input().split())
+t = int(input())
+graph = [[] *(v+1) for _ in range(v+1)]
+distance = [INF] * (v+1)
+
+for _ in range(v):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, node = heapq.heappop(q)
+        if distance[node] < dist:
+            continue
+        for i in graph[node]:
+            cost = dist + i[1]
+        if cost < distance[i[0]]:
+            distance[i[0]] = cost
+            heapq.heappush(q, (cost, i[0]))
+
+dijkstra(t)
+
+for i in range(1, v+1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
+
+# print(graph)
