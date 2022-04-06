9e99a236cc8ccb68e88f8325edd02da8df8e707b
ggongdori <soominlee0410@gmail.com>
2022-04-06 12:07:33 +0900

    effective_currency.py solution final

--- dp_0406/effective_currency.py
+++ dp_0406/effective_currency.py
@@ -4,7 +4,22 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-currency = list(map(int, input().split()))
+currency = []
+for _ in range(n):
+    currency.append(int(input()))
+dp = [10001] * (m+1)
+
+dp[0] = 0
+
+for i in range(n):
+    for j in range(currency[i], m+1):
+        if dp[j-currency[i]] != 10001:
+            dp[j] = min(dp[j], dp[j-currency[i]] + 1)
+
+if dp[m] == 10001:
+    print(-1)
+else:
+    print(dp[m])
 
 
 
