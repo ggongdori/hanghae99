6e35810f9b9ff28a1c6527e3be29a4a83bde11e9
ggongdori <soominlee0410@gmail.com>
2022-04-06 11:10:53 +0900

    ants solution added

--- dp_0406/ants.py
+++ dp_0406/ants.py
@@ -2,3 +2,16 @@ import sys
 
 input = sys.stdin.readline
 
+n = int(input())
+
+lst = list(map(int, input().split()))
+
+dp = [0] * 10
+
+dp[0] = lst[0]
+dp[1] = max(lst[0], lst[1])
+
+for i in range(2,n):
+    dp[i] = max(dp[i-1], dp[i-2]+ lst[i])
+
+print(dp[n-1])
