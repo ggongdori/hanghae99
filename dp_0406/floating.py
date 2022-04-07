3e922db4c6cad9acc3e539ed2c4fe8d20d4bbe54
ggongdori <soominlee0410@gmail.com>
2022-04-07 12:18:49 +0900

    0407 initial commit

---
+++ dp_0406/floating.py
@@ -0,0 +1,10 @@
+a = float(0.1)
+
+sum_a = float(0.0)
+for _ in range(1000):
+    sum_a += a
+    # sum_b += b
+
+print(sum_a)
+# print(sum_b)
+
---
+++ test_0407/prac.py
@@ -0,0 +1,6 @@
+a = float(0.1)
+sum_a = 0
+for _ in range(100):
+    sum_a += a
+print(sum_a)
+#9.99999999999998
